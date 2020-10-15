736843960d382446c11a0514927107f3d85d413a
Angelo Cutaia <angelo.cutaia@linksfoundation.com>
2020-10-15 10:33:10 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
     install_requires=[
         'aioserial>=1.3.0',
         'asyncpg>=0.21.0',
-        'bitarray>=1.5.1',
+        'bitarray>=1.5.3',
         'uvloop>=0.14.0'
     ],
     include_package_data=True,
